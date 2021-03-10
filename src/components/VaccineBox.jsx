7f2cd3059de7289cacfdb973a6d9016b89270139
Luka <bartul123@outlook.com>
2021-03-10 23:34:59 +0100

    Create VaccineBox.jsx

---
+++ src/components/VaccineBox.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./VaccineBox.css";
+
+export default function VaccineBox(props) {
+  console.log(props);
+  const {
+    candidate,
+    details,
+    institutions,
+    mechanism,
+    sponsors,
+    trialPhase,
+  } = props.data;
+  return (
+    <div className="VaccineBox">
+      <div className="VaccineBox-table">
+        <span>
+          Candidate: <span>{candidate}</span>
+        </span>
+        <span>
+          Institutions: <span>{institutions}</span>
+        </span>
+        <span>
+          Mechanism: <span>{mechanism}</span>
+        </span>
+        <span>
+          Sponsors: <span>{sponsors}</span>
+        </span>
+        <span>
+          Trial Phase: <span>{trialPhase}</span>
+        </span>
+      </div>
+      <span
+        dangerouslySetInnerHTML={{ __html: details }}
+        className="VaccineBox-details"
+      ></span>
+    </div>
+  );
+}
