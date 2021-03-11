e665d58b6af6f1bb11e79368897736b3096e7112
Luka <bartul123@outlook.com>
2021-03-11 17:33:28 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./Header.css";
 
 import { rotating } from "../images/index";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header className="Header">
-        {" "}
-        <span className="Header-span-c">C</span>
-        <img className="image" src={rotating} alt="corona-img" />{" "}
-        <span className="Header-span-vid">VID - 19</span>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className="Header">
+      {" "}
+      <span className="Header-span-c">C</span>
+      <img className="image" src={rotating} alt="corona-img" />{" "}
+      <span className="Header-span-vid">VID - 19</span>
+    </header>
+  );
+};
+
+export default Header;
