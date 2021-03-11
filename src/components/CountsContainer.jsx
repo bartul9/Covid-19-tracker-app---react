3c2393b40844c5dd095950fade84af69f595e223
Luka <bartul123@outlook.com>
2021-03-11 17:33:42 +0100

    Update CountsContainer.jsx

--- src/components/CountsContainer.jsx
+++ src/components/CountsContainer.jsx
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Counts from "./Counts";
 import LastUpdate from "./LastUpdate";
 import Spinner from "./Spinner";
 
+import { recoveredImg, death, cough } from "../images/index";
+
 import "./CountsContainer.css";
+
 import { inject, observer } from "mobx-react";
 
-class CountsContainer extends Component {
-  render() {
-    return !this.props.store.todos.countriesData.lastUpdate ? (
+const CountsContainer = inject("store")(
+  observer(({ store }) => {
+    const {
+      lastUpdate,
+      confirmed,
+      deaths,
+      recovered,
+    } = store.todos.countriesData;
+    return !lastUpdate ? (
       <Spinner />
     ) : (
       <section className="CountsContainer">
         {" "}
+        <span id="countries-href"></span>
         <LastUpdate />
         <div>
           <Counts
@@ -22,7 +32,8 @@ class CountsContainer extends Component {
             colorStyle={"orange"}
             backgroundColor={"#ffa60030"}
             text={"active cases of"}
-            numbers={this.props.store.todos.countriesData.confirmed}
+            numbers={confirmed}
+            icon={cough}
           />
           <Counts
             key={2}
@@ -30,7 +41,8 @@ class CountsContainer extends Component {
             text={"recoveries from"}
             colorStyle={"#ff6961"}
             backgroundColor={"#ff696120"}
-            numbers={this.props.store.todos.countriesData.deaths}
+            numbers={deaths}
+            icon={death}
           />
           <Counts
             key={3}
@@ -38,12 +50,13 @@ class CountsContainer extends Component {
             text={"deaths caused by"}
             colorStyle={"#32CD32"}
             backgroundColor={"#32cd3220"}
-            numbers={this.props.store.todos.countriesData.recovered}
+            numbers={recovered}
+            icon={recoveredImg}
           />
         </div>{" "}
       </section>
     );
-  }
-}
+  })
+);
 
-export default inject("store")(observer(CountsContainer));
+export default CountsContainer;
