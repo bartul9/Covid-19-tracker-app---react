522daddf20130dbfc96c745e5c10b4176591cd10
Luka <bartul123@outlook.com>
2021-03-11 17:34:13 +0100

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,48 +1,40 @@
-import React from "react";
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./AppBar.css";
 
-export default class AppBar extends Component {
-  constructor(props) {
-    super(props);
+import { totop } from "../images/index";
 
-    this.state = {
-      nav: false,
-    };
+const Bar = () => {
+  const [nav, setNav] = useState(false);
 
-    this.navbarRef = React.createRef();
-  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("scroll");
-  }
-  handleScroll = () => {
+  const handleScroll = () => {
     if (window.pageYOffset >= 128) {
-      if (!this.state.nav) {
-        this.setState({ nav: true });
-      }
+      setNav(true);
     } else {
-      if (this.state.nav) {
-        this.setState({ nav: false });
-      }
+      setNav(false);
     }
   };
-  render() {
-    return (
-      <nav
-        ref={this.navbarRef}
-        id="navbar"
-        className={`AppBar ${this.state.nav && "sticky"}`}
-      >
-        <ul>
-          <li>Countries-Data</li>
-          <li>Vaccines</li>
-        </ul>{" "}
-      </nav>
-    );
-  }
-}
+
+  return (
+    <nav id="navbar" className={`AppBar ${nav && "sticky"}`}>
+      <ul>
+        <li>
+          <a href="#countries-href">Countries</a>
+        </li>
+        <li>
+          <a href="#VaccineDetails-h2">Vaccine</a>
+        </li>
+      </ul>{" "}
+      {nav && (
+        <a href="#top">
+          <img className="BackToTop" src={totop} alt="backtotop" />
+        </a>
+      )}
+    </nav>
+  );
+};
+export default Bar;
